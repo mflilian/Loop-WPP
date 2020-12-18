1c977f6006255fd97f3a87bdc5b6dfd2cd84f572
mflilian <mayhhara@hotmail.com.br>
2020-12-18 01:33:54 -0300

    adding code

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Loop-WPP
+<h1 align="center">
+<p align="center"> - Loop-Wpp - <p>
+</h1>
+
+Bot que envia mensagens em loop pelo WhatsApp.
+
+### Instalação
+
+```bash
+# Clonar o repositório
+$ git clone https://github.com/mflilian/Loop-WPP
+
+# Entrar na pasta do repositório
+$ cd Loop-WPP
+
+# Instalar as dependências
+$ npm install 
+
+# Executar o servidor
+$ npm start
+
+```
+```bash
+Caso não funcione e alguma dependência fique faltando, tente instalar as dependências separadamente:
+
+# Instalar as dependências
+$ npm init -y
+$ npm i pupperteer 
+
+# Executar o servidor
+$ node index.js
+
+```
+
+Se, por ventura, esteja utilizando outro gerenciador de pacotes apenas altere o npm para seu respectivo instalador.
+
+
+
+#### Tecnologias e pacotes utilizados
+- Node.js
+- Git
+- pupperteer
+
---
+++ index.js
@@ -0,0 +1,49 @@
+const puppeteer = require("puppeteer");
+
+// Login Function Logic
+(async function main() {
+  try {
+    // Configures puppeteer
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+    );
+
+    //Navigates to Whatsapp
+    await page.goto("https://web.whatsapp.com/");
+
+    // //Searches person by title
+    await page.waitForSelector("._1hI5g"); //Add selector by the css class who was identified by html code
+    await delay(5000);
+
+    //Change to contact you want to send messages to
+    const contactName = "Monsieur Amancio";
+    await page.click(`span[title='${contactName}']`);
+    await page.waitForSelector("._1C6Zl"); //Add selector by the css class who was identified by html code
+
+    //Finds the message bar and focuses on it
+    const editor = await page.$("div[data-tab='6']"); //Add message bar by the css class who was identified by html code
+
+    //Amount of messages you want to send
+    const amountOfMessages = 500;
+
+    //Loops through cycle of sending message
+    for (var i = 0; i < amountOfMessages; i++) {
+      await page.evaluate(() => {
+        const message = "melhor pessoa em linha reta";
+        document.execCommand("insertText", false, message);
+      });
+      await page.click("span[data-testid='send']"); //Add send message button by the css class who was identified by html code
+      await delay(500);
+    }
+  } catch (e) {
+    console.error("error mine", e);
+  }
+})();
+
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
